05174788336b638ce895dbdb1ac8c45e15de14be
Sricharan Ramesh <charan1952001@gmail.com>
2021-02-09 17:09:38 +0530

    feat: add React project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import ContentEditor from "./Components/Editor";
+function App() {
+  return (
+    <div className="">
+      <ContentEditor />
+    </div>
+  );
+}
+
+export default App;
