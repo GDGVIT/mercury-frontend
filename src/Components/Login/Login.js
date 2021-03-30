65f1d07eeb18fd7f921306433ea0a3c92cff6148
Sricharan Ramesh <charan1952001@gmail.com>
2021-03-30 16:24:42 +0530

    fix: make Login page responsive

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -8,9 +8,9 @@ const Login = () => {
     <div style={{ height: '100vh' }}>
       <Header />
       <div className='login'>
-        <div style={{ marginLeft: '100px', width: '40%', maxWidth: '400px' }}>
+        <div className='login-body'>
           <div className='login-content'>
-            <h1>LOG IN</h1>
+            <h1>LOGIN</h1>
             <h4>Do not have an account? <a href='/register'>Create one</a></h4>
           </div>
           <div className='form'>
@@ -18,7 +18,7 @@ const Login = () => {
             <input type='password' className='text-input' placeholder='Password' />
             <div>
               <input type='radio' className='radio-input' id='password' name='password' />
-              <label htmlFor='password'>Show Password</label>
+              <label className='showPassword' htmlFor='password'>Show Password</label>
             </div>
             <button type='submit'>LOG IN</button>
           </div>
