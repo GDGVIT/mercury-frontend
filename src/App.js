6cb4e2ba55af6477ffe77815855d2895b61d48f2
Sricharan Ramesh <charan1952001@gmail.com>
2021-05-03 12:19:33 +0530

    feat: add logo

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Landing from './Components/Landing/Landing'
 import Login from './Components/Login/Login'
 // import Footer from './Components/Footer/Footer'
 // import Register from './Components/Register/Register'
+import DnDEnditor from './Components/DnDEditor/DnDEditor'
 
 function App () {
   return (
@@ -23,6 +24,9 @@ function App () {
           </Route>
           <Route exact path='/mail'>
             <ContentEditor />
+          </Route>
+          <Route path='/dnd'>
+            <DnDEnditor />
           </Route>
           {/* <Route path='/footer'>
             <Footer />
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import '../Landing/Landing.css'
+import logo from '../../assets/logo.svg'
 
 const Header = () => {
   return (
     <div className='header'>
+      <img src={logo} className='logo' />
       <a href='/'><h1 className='title'>Mercury Mailer</h1></a>
     </div>
   )
