1bf131d12acfc5ce197b7fbbbe38603d0f9f7aaa
Sricharan Ramesh <charan1952001@gmail.com>
2021-06-27 17:30:02 +0530

    feat: add logout button in header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,25 @@
 import React from 'react'
+import { useHistory } from 'react-router'
 import '../Landing/Landing.css'
 import logo from '../../assets/logo.svg'
 
 const Header = () => {
+  const history = useHistory()
+
+  const handleClick = () => {
+    window.localStorage.clear()
+    history.push('/')
+  }
   return (
     <div className='header'>
       <a href='/' style={{ display: 'flex' }}>
         <img src={logo} alt='logo' className='logo' />
         <h1 className='title'>Mercury Mailer</h1>
       </a>
+      {
+        (window.localStorage.getItem('token') !== null && window.localStorage.getItem('token') !== undefined) &&
+          <div onClick={handleClick} className='logout'>Logout</div>
+      }
     </div>
   )
 }
